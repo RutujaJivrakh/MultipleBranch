24d77e6d6a8930a7aff95991d450167e04b10bb6
RutujaJivrakh <jivrakhrb@gmail.com>
2022-11-14 17:15:52 +0530

    Project Added

---
+++ src/main/java/com/example/GitMultipleBranchProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitMultipleBranchProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitMultipleBranchProjectApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/Welcome/Welcome.java
@@ -0,0 +1,10 @@
+package com.example.Welcome;
+
+public class Welcome {
+
+	public String welcome()
+	{
+		return null;
+		
+	}
+}
